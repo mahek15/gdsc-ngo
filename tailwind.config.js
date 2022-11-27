0ac1bccdbafe42a7b8f1b231d220a53419cdd52e
unknown <mahekmj123@gmail.com>
2022-11-27 23:47:40 +0530

    font added and mission and vision pages updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily:{
-        montserrat : ['Montserrat','sans-serif'],
+        montserrat : [' "Montserrat" ','sans-serif'],
       }
     },
   },
