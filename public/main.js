b819c3182dd5c039b12c65e11afd7f277cf25a3e
unknown <mahekmj123@gmail.com>
2022-12-29 12:04:29 +0530

    carousel updated

---
+++ public/main.js
@@ -0,0 +1,37 @@
+currentSlideID = 1;
+
+sliderElement = document.getElementById('slider');
+totalSlides = sliderElement.childElementCount;
+
+function next(){
+if(totalSlides > currentSlideID)
+{
+    currentSlideID++;
+    showSlide()
+}
+    
+}
+
+function prev()
+{
+if (currentSlideID > 1 )
+{
+    currentSlideID--;
+    showSlide()
+}
+}
+function showSlide() 
+{
+    slides = document.getElementById('slider').getElementsByTagName('li')
+    for(let index = 0 ; index < totalSlides;index++)
+    {
+        const element = slides[index];
+        if(currentSlideID === index+1)
+        {
+            element.classList.remove('hidden')
+        }
+        else{
+            element.classList.add('hidden')
+        }
+    }
+}
